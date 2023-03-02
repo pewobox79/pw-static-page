0003a3effc63f62a0e7a98ad2b832f43aa18bc3f
Peter Wolf <pewobox79@Peters-Laptop.fritz.box>
2023-03-02 17:59:17 +0100

    origin added

--- src/components/AboutMe/EducationComponents/EducationContainer.jsx
+++ src/components/AboutMe/EducationComponents/EducationContainer.jsx
@@ -13,6 +13,7 @@ export default function EducationContainer() {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
+                'Origin': 'https://webdeveloper-peterwolf.com'
             },
         };
 
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,5 @@ export default defineConfig({
   server: {
     port: '3000',
   },
-  paths:{
 
-  }
 })
