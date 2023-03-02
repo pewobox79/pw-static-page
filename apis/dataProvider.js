4ff92bb6b0a2de0d669560321be3b5cb9da20b5b
Peter Wolf <pewobox79@Peters-Laptop.fritz.box>
2023-03-02 18:10:26 +0100

    url update temp

--- apis/dataProvider.js
+++ apis/dataProvider.js
@@ -1,5 +1,5 @@
-const backendURL = import.meta.env.VITE_VERCEL_DRUPAL_BACKEND_URL
-
+/*const backendURL = import.meta.env.VITE_VERCEL_DRUPAL_BACKEND_URL*/
+const backendURL = "https://pewobox.com/api/"
 export async function getDataFromBackend(searchString){
    const response =await fetch(backendURL+searchString)
     const data = await response.json()
