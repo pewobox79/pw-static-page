87a7d9c7d26d53c1c7d1f5ddfdf8a2325d1229eb
Peter Wolf <pewobox79@gmail.com>
2023-04-18 11:30:59 +0200

    updated experience date

--- src/components/AboutMe/ExperienceComponents/ExperienceItem.jsx
+++ src/components/AboutMe/ExperienceComponents/ExperienceItem.jsx
@@ -4,11 +4,22 @@ import ExpVerticalLine from "./ExpVerticalLine.jsx";
 import JobDetails from "./JobDetails.jsx";
 
 export default function ExperienceItem({jobTitle, startDate, endDate, companyName, educationGrade,}) {
+
+    const today = new Date()
+    const year = today.getFullYear()
+
+    let myEndDate = ""
+
+    if(year.toString() === endDate){
+        myEndDate = "today"
+    }else{
+        myEndDate = endDate
+    }
+
     return (
         <div className={style.experienceItem}>
             <ExpVerticalLine />
-
-            <Chip label={`${startDate} - ${endDate}`} />
+            <Chip label={`${startDate} - ${myEndDate}`} />
             <JobDetails
                 companyName={`${companyName}`}
                 jobTitle={jobTitle}
