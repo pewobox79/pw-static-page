92bbd3e9ba0a5a0b9188f368d7b37f99f79289be
Peter Wolf <pewobox79@gmail.com>
2023-04-21 12:02:51 +0200

    chart query updated

--- src/lib/DashboardDataHelper.js
+++ src/lib/DashboardDataHelper.js
@@ -12,3 +12,26 @@ export async function getToDoList(){
     }
 
 }
+
+
+export async function getCryptoChartValue(query){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'f2f2dfce82mshcb64156556ea80ap1968b8jsn5e6669fe879d',
+            'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+        }
+    };
+
+    try {
+
+
+        const response = await fetch(`https://alpha-vantage.p.rapidapi.com/query?market=CNY&function=DIGITAL_CURRENCY_WEEKLY&symbol=${query}`, options)
+        const data = await response.json()
+        return data
+    }catch(err){
+
+        console.log("fetch failed", err)
+
+    }
+}
