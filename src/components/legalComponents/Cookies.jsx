3579bf4de09e4a9078d006318ace81040af4aea0
Peter Wolf <pewobox79@gmail.com>
2023-05-24 14:35:32 +0200

    local storage delete updated in cookies

--- src/components/legalComponents/Cookies.jsx
+++ src/components/legalComponents/Cookies.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from '../../styling/Imprint.module.css'
+import DeleteLocalStorageData from "../assetsComponents/Buttons/DeleteLocalStorageData.jsx";
 
 const Cookies=()=>{
     return(
         <div className={styles.cookies}>
-        <h1>we don´t set cookies at all!</h1>
+        <h1>For us it is important that your data is safe!</h1>
+            <h3>Website Cookies</h3>
+            <p>This website is not storing or setting any COOKIES - and will never!</p>
+            <h3>Demo section</h3>
+            <p>My website offers a DEMO section to which I will provide access throughout an restricted area.<br/>
+            To enter the DEMO you have to register via the provided form first.<br/>
+            All provided data of you will be store in the local storage of your personal browser, to which only you have access to.<br/>This website cannot share any data with me or other user, because local storage is only accessible via your personal device.<br/>
+            If you want to delete your local storage of your browser for the DEMO, you can do it by using the below button.</p>
+
+            <DeleteLocalStorageData/>
+
         </div>
     )
 }
